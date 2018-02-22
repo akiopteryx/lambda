a8ff3721aa60665431bc34e2a4c231caa295eb89
Aki Watanabe <akinopteryx@gmail.com>
2018-02-21 20:47:43 -0500

    Update lasec.R

--- R/lasec.R
+++ R/lasec.R
@@ -68,10 +68,10 @@ for(i in 1:iter) {
 }
 
 matrix.chosen <- matrix(matrix.chosen, nrow=iter, byrow=T)
-matrix.fit <- 1-matrix(matrix.pss, nrow=iter, byrow=T)
-median.fit <- apply(matrix.pss, 2, median)
-matrix.fit.cs <- 1-matrix(matrix.pss.cs, nrow=iter, byrow=T)
-median.fit.cs <- apply(matrix.pss.cs, 2, median)
+matrix.pss <- 1-matrix(matrix.pss, nrow=iter, byrow=T)
+median.pss <- apply(matrix.pss, 2, median)
+matrix.pss.cs <- 1-matrix(matrix.pss.cs, nrow=iter, byrow=T)
+median.pss.cs <- apply(matrix.pss.cs, 2, median)
 
 # PLOT PROC SS CURVE #
 pdf("LaSEC_SamplingCurve_Shape.pdf")
@@ -96,10 +96,10 @@ plot(median.fit.cs, xlim=c(0, n.lm), ylim=c(0, 1), type="l", col="black", lwd=3,
 dev.off()
 
 # OUTPUT #
-output$fit <- matrix.fit
-output$median.fit <- median.fit
-output$maxpss.landmark <- table(lm.maxpss)
-output$minpss.landmark <- table(lm.minpss)
-output$pss.cs <- median.fit.cs
+output$fit <- matrix.pss
+output$median.fit <- median.pss
+output$maxfit.landmark <- table(lm.maxpss)
+output$minfit.landmark <- table(lm.minpss)
+output$fit.cs <- median.pss.cs
 return(output)		
 }
